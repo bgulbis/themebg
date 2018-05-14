d1fccc7a41de35ea3dc10c874060aa8ac764d682
bgulbis <brian.gulbis@gmail.com>
2018-05-14 14:27:51 -0500

    update legend title

--- R/themebg.R
+++ R/themebg.R
@@ -16,21 +16,27 @@
 #' @export
 #'
 theme_bg <- function(base_size = 11, base_family = "", xticks = TRUE, yticks = TRUE) {
-    p <- ggplot2::theme_bw(base_family = base_family, base_size = base_size) +
-        ggplot2::theme(legend.background = ggplot2::element_blank(),
-              legend.key = ggplot2::element_blank(),
-              legend.text = ggplot2::element_text(color = "grey35"),
-              panel.background = ggplot2::element_blank(),
-              panel.border = ggplot2::element_blank(),
-              strip.background = ggplot2::element_blank(),
-              strip.text = ggplot2::element_text(color = "grey35"),
-              plot.background = ggplot2::element_blank(),
-              panel.grid = ggplot2::element_blank(),
-              axis.line = ggplot2::element_line(color = "grey85"),
-              axis.text = ggplot2::element_text(color = "grey35"),
-              axis.title = ggplot2::element_text(color = "grey35"),
-              axis.ticks = ggplot2::element_line(color = "grey35"),
-              plot.caption = element_text(color = "gray35"))
+    p <- ggplot2::theme_bw(
+        base_family = base_family,
+        base_size = base_size
+    ) +
+        ggplot2::theme(
+            legend.background = ggplot2::element_blank(),
+            legend.key = ggplot2::element_blank(),
+            legend.text = ggplot2::element_text(color = "grey35"),
+            legend.title = ggplot2::element_text(color = "grey35"),
+            panel.background = ggplot2::element_blank(),
+            panel.border = ggplot2::element_blank(),
+            strip.background = ggplot2::element_blank(),
+            strip.text = ggplot2::element_text(color = "grey35"),
+            plot.background = ggplot2::element_blank(),
+            panel.grid = ggplot2::element_blank(),
+            axis.line = ggplot2::element_line(color = "grey85"),
+            axis.text = ggplot2::element_text(color = "grey35"),
+            axis.title = ggplot2::element_text(color = "grey35"),
+            axis.ticks = ggplot2::element_line(color = "grey35"),
+            plot.caption = element_text(color = "gray35")
+        )
 
     if (!xticks) p <- p + theme(axis.ticks.x = ggplot2::element_blank())
     if (!yticks) p <- p + theme(axis.ticks.y = ggplot2::element_blank())
