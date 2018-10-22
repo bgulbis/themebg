0520f639eb2ee47698c0f831c5b9ac8cb06df30b
Brian Gulbis <brian.gulbis@gmail.com>
2018-10-22 14:41:34 -0500

    add theme for better b&w printing / copying

--- R/themebg.R
+++ R/themebg.R
@@ -43,3 +43,46 @@ theme_bg <- function(base_size = 11, base_family = "", xticks = TRUE, yticks = T
     p
 }
 
+#' Custom ggplot2 theme for black and white printing
+#'
+#' This is a custom ggplot2 theme loosely based on recommendations of Edward
+#' Tufte, Stephen Few, and others. The panel background is blank and gridlines
+#' are removed. In contrast to theme_bg, this theme uses dark axis colors to
+#' improve black and white printing or copying.
+#'
+#' @param base_size base font size
+#' @param base_family base font family
+#' @param xticks logical indicating whether tick marks should be displayed on
+#'   the x-axis
+#' @param yticks logical indicating whether tick marks should be displayed on
+#'   the y-axis
+#'
+#' @export
+#'
+theme_bg_print <- function(base_size = 11, base_family = "", xticks = TRUE, yticks = TRUE) {
+    p <- ggplot2::theme_bw(
+        base_family = base_family,
+        base_size = base_size
+    ) +
+        ggplot2::theme(
+            legend.background = ggplot2::element_blank(),
+            legend.key = ggplot2::element_blank(),
+            legend.text = ggplot2::element_text(color = "black"),
+            legend.title = ggplot2::element_text(color = "black"),
+            panel.background = ggplot2::element_blank(),
+            panel.border = ggplot2::element_blank(),
+            strip.background = ggplot2::element_blank(),
+            strip.text = ggplot2::element_text(color = "black"),
+            plot.background = ggplot2::element_blank(),
+            panel.grid = ggplot2::element_blank(),
+            axis.line = ggplot2::element_line(color = "black"),
+            axis.text = ggplot2::element_text(color = "black"),
+            axis.title = ggplot2::element_text(color = "black"),
+            axis.ticks = ggplot2::element_line(color = "black"),
+            plot.caption = element_text(color = "black")
+        )
+
+    if (!xticks) p <- p + theme(axis.ticks.x = ggplot2::element_blank())
+    if (!yticks) p <- p + theme(axis.ticks.y = ggplot2::element_blank())
+    p
+}
